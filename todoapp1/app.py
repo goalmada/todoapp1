e18d58e66db0c1a97484ee867ed14c38147253f8
goalmada <48073770+goalmada@users.noreply.github.com>
2020-03-18 13:43:09 -0700

    Add files via upload

---
+++ todoapp1/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://diegoalmada@localhost:5432/todoapp1'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify({
+        'description': todo.description
+    })
+
+@app.route('/')
+def index():
+    return render_template('index1.html', data=Todo.query.all()
+    )
+
